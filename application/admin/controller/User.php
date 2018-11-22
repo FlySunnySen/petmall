ace8b69c28f18a83fab65e5e2f5e3ecc9bab934f
zhouchusen <274818439@qq.com>
2018-11-22 16:36:40 +0800

    layui nice

--- application/admin/controller/User.php
+++ application/admin/controller/User.php
@@ -39,26 +39,51 @@ class User extends Common
 	}
 
 	public function editUserPwd(){
-         halt('hhh');
+        $map['Uid'] = input('Uid');
+        $old_pwd = Db::name('user')->where($map)->value('user_pwd');
+        $new_pwd = md5(md5(input('pwd')));
+        //如果新密码与旧密码相同
+        if($old_pwd == $new_pwd){
+             $arr = array(
+                   'code' => 304,
+                   'tip'  => '与旧密码相同'
+             );
+             return json_encode($arr);
+        }
+        $rst = Db::name('user')->where($map)->setField('user_pwd',$new_pwd);
+        if($rst){
+        	 $arr = array(
+                   'code' => 200,
+                   'tip'  => '修改成功'
+             );       	
+        }else{
+        	 $arr = array(
+                   'code' => 500,
+                   'tip'  => '修改失败'
+             );	
+        }
+        return json_encode($arr);
 	}
     //禁用用户
 	public function softDelUser(){
         $id = input('id');
         $action = input('action');
+
         if($action == 1){
         	$status = 0;
         }else{
         	$status = 1;
         }     
-        $data['is_delete'] = $status; 
+        $data['is_delete'] = $status;
         $rst = Db::name('user')->where('Uid','=',$id)->setField($data);
 
         
         if($rst){
-            $this->success('操作成功','user/index');
+            return json_encode(1);
         }else{
-            $this->error('操作失败');
+             return json_encode(0);
         }
+
 	}
 
 
